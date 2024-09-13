db0130a491bde6ad7a5fcdfa4b603999430f809f
PixieCatSupreme <cynthia.steenvoorden@gmail.com>
2024-09-13 15:25:22 +0200

    Added more enemy locations

--- worlds/melvor/Data/Regions.py
+++ worlds/melvor/Data/Regions.py
@@ -14,3 +14,19 @@ all_regions = [
     "Combat Area - Dragon Valley",
     "Combat Area - Elerine Battlegrounds",
 ]
+
+demo_regions = [
+    "Combat",
+    "Combat Area - Farmlands",
+    "Combat Area - Goblin Village",
+    "Combat Area - Graveyard",
+    "Combat Area - Sandy Shores",
+    "Combat Area - Wet Forest",
+    "Combat Area - Bandit Hideout",
+    "Combat Area - Giant Dungeon",
+    "Combat Area - Icy Hills",
+    "Combat Area - Castle of Kings",
+    "Combat Area - Wizard Tower",
+    "Combat Area - Dragon Valley",
+    "Combat Area - Elerine Battlegrounds",
+]
